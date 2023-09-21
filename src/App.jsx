9d5d85880e128d93cfe5c64205529d1496eea84c
AlirezaHK91 <113583919+AlirezaHK91@users.noreply.github.com>
2023-09-21 10:14:54 +0200

    Added BrowserRouter, FetchExample

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import * as React from 'react';
 import NavBar from './components/Navbar';
-import User from './components/Product'
-
-
+import HomePage from './components/Pages/HomePage';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 function App() {
 
   return (
-    <>
-    <NavBar className="navbar"/>
-    <User></User>
-    </>
+  <BrowserRouter>
+    <NavBar/>
+      <Routes>
+        <Route path="/" element={<HomePage/>}/>
+
+      </Routes>
+  </BrowserRouter>
   );
 } 
 export default App;
