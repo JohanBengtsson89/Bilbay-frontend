33579770b82f690b1ac6c60f9e98d2154aff3fab
Aldren <Aldrenpeshaia@hotmail.com>
2023-09-23 16:59:36 +0200

    In this commit me and Alireza have added footer to all routes in the website and also installed Tailwindcss.

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <div className=" position absolute bottom-0 w-full py-10 text-center bg-[#A3B8CB] text-black text-base">
+      Copyright © 2023 Bilbay Inc.
+    </div>
+  );
+}
+
+export default Footer;
