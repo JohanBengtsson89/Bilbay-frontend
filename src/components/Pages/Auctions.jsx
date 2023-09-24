bde0a4481f1793915f3ec7ddb43e5db26c18cc33
Christoffer Wallberg <christoffer.wallberg85@gmail.com>
2023-09-24 23:27:22 +0200

    gets all auctions from the backend and presenting it

--- src/components/Pages/Auctions.jsx
+++ src/components/Pages/Auctions.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 const Auctions = () => {
-  return (
-    <div>Auctions</div>
-  )
-}
+  const [auctions, setAuctions] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-export default Auctions
+  useEffect(() => {
+    const apiUrl = "http://localhost:8080/api/auctions";
+
+    axios
+      .get(apiUrl)
+      .then((response) => {
+        setAuctions(response.data);
+        setLoading(false);
+        setError(null);
+      })
+      .catch((err) => {
+        setLoading(false);
+        setError(err);
+      });
+  }, []);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Error: {error.message}</p>;
+  }
+
+  return (
+    <div>
+      Auctions
+      <ul>
+        {auctions.map((auction) => (
+          <li key={auction.id}>
+            <h3>ID: {auction.id}</h3>
+            <p>User: {auction.user}</p>
+            <p>Product: {auction.product}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Auctions;
