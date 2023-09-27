20e8728a312c9a7cddfa6517b70b31ab83fe0642
Aldren <Aldrenpeshaia@hotmail.com>
2023-09-27 18:15:42 +0200

    I have been trying to wrok out my carousel, which it should slide for every picture that exists and right now i have 5 pictures i want it slide to, it is working but i wanna set the first picture as default so whenever someone goes into aboutus it should be right there and not in 5 seconds.

--- src/components/Pages/AboutUsPage.jsx
+++ src/components/Pages/AboutUsPage.jsx
@@ -1,14 +1,60 @@
-import Luxury from "../../assets/";
+// Initialization for ES Users
+import { Carousel, initTE } from "tw-elements";
+
+initTE({ Carousel });
+
+import image1 from "../../assets/Luxury.png";
+import image2 from "../../assets/Luxury2.png";
+import image3 from "../../assets/Luxury3.png";
+import image4 from "../../assets/Luxury4.png";
+import image5 from "../../assets/Luxury5.png";
 
 export default function AboutUsPage() {
   return (
-    <div>
-      <div className="mx-auto bg-black flex align">
-        <img className={Luxury} src="" alt="luxury" />
-        <div className="text-white mx-auto">
-          <div>dsada</div>
+    <div
+      id="carouselExampleSlidesOnly"
+      class="relative"
+      data-te-carousel-init
+      data-te-ride="carousel"
+    >
+      <div class="relative w-full overflow-hidden after:clear-both after:block after:content-['']">
+        <div
+          class="relative float-left -mr-[100%] hidden w-52 transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
+          data-te-carousel-item
+          data-te-carousel-active
+        >
+          <img src={image1} class="" alt="Wild Landscape" />
+        </div>
+
+        <div
+          class="relative float-left -mr-[100%] hidden w-[350px] transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
+          data-te-carousel-item
+        >
+          <img src={image2} class="" alt="Camera" />
+        </div>
+        <div
+          class="relative float-left -mr-[100%] hidden w-[350px] transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
+          data-te-carousel-item
+        >
+          <img src={image3} class="" alt="Camera" />
+        </div>
+        <div
+          class="relative float-left -mr-[100%] hidden w-[350px] transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
+          data-te-carousel-item
+        >
+          <img src={image4} class="" alt="Camera" />
+        </div>
+        <div
+          class="relative float-left -mr-[100%] hidden w-[350px] transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
+          data-te-carousel-item
+        >
+          <img src={image5} class="" alt="Camera" />
         </div>
       </div>
     </div>
   );
 }
+<script
+  type="text/javascript"
+  src="../node_modules/tw-elements/dist/js/tw-elements.umd.min.js"
+></script>;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/tw-elements/dist/js/**/*.js",
+  ],
   theme: {
     extend: {},
     screens: {
@@ -20,5 +24,6 @@ export default {
       // => @media (max-width: 639px) { ... }
     },
   },
-  plugins: [],
+  plugins: [require("tw-elements/dist/plugin.cjs")],
+  darkMode: "class",
 };
