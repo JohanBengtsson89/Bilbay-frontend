72737c94afd4314bf4ef9378a63710744eb1cbf2
AlirezaHK91 <kasra_tehran101@hotmail.com>
2023-10-02 22:20:59 +0200

    added dynamic functions

--- src/components/Favourites.jsx
+++ src/components/Favourites.jsx
@@ -1,10 +1,7 @@
-import React, { useState } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faHeart as faHeartSolid,
-  faHeart as faHeartOutline,
-} from "@fortawesome/free-solid-svg-icons";
-import "./Favourite.css";
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart as faHeartSolid, faHeart as faHeartOutline } from '@fortawesome/free-solid-svg-icons';
+import './Favourite.css';
 
 const products = [
   {
@@ -16,41 +13,103 @@ const products = [
     name: "Product 2",
     price: 29.99,
     description: "Somthing",
-  },
+  }
+  
 ];
 
 function Favorite() {
+  
   const [favorites, setFavorites] = useState([]);
+  const [userId, setUserId] = useState(null);
+  const [products, setProducts] = useState([]);
 
-  const addToFavorites = (product) => {
+  
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await axios.get('https://api.example.com/products');
+        setProducts(response.data);
+      } catch (error) {
+        console.error('Error fetching products:', error);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+  
+  useEffect(() => {
+    const fetchFavorites = async () => {
+      try {
+        // Include the user ID when fetching favorites
+        const response = await axios.get(`https://api.example.com/favorites?userId=${userId}`);
+        setFavorites(response.data);
+      } catch (error) {
+        console.error('Error fetching favorites:', error);
+      }
+    };
+
+    if (userId) {
+      fetchFavorites();
+    }
+  }, [userId]);
+
+  const addToFavorites = async (productId) => {
+    try {
+      // Include the user ID and product ID when adding to favorites
+      await axios.post('https://api.example.com/add-to-favorites', { userId, productId });
+      const newFavorites = [...favorites, productId];
+      setFavorites(newFavorites);
+    } catch (error) {
+      console.error('Error adding to favorites:', error);
+    }
+  };
+  
+  const removeFromFavorites = async (productId) => {
+    try {
+      // Include the user ID and product ID when removing from favorites
+      await axios.delete('https://api.example.com/remove-from-favorites', {
+        data: { userId, productId },
+      });
+
+      const newFavorites = favorites.filter((item) => item !== productId);
+      setFavorites(newFavorites);
+    } catch (error) {
+      console.error('Error removing from favorites:', error);
+    }
+  };
+  /* const addToFavorites = (product) => {
     if (!favorites.includes(product)) {
+      
       const newFavorites = [...favorites, product];
       setFavorites(newFavorites);
     }
   };
 
+  
   const removeFromFavorites = (product) => {
     const newFavorites = favorites.filter((item) => item !== product);
     setFavorites(newFavorites);
-  };
+  }; */
 
   return (
     <div>
-      <h1 className="products">Products</h1>
+      
+      <h1 className='products' >Products</h1>
       <ul>
         {products.map((product, index) => (
           <li key={index}>
             {product.name}
-            {favorites.includes(product.name) ? (
+            {favorites.includes(product.id) ? (
               <FontAwesomeIcon
                 icon={faHeartSolid}
-                onClick={() => removeFromFavorites(product.name)}
+                onClick={() => removeFromFavorites(product.id)}
                 className="heart-icon red"
               />
             ) : (
               <FontAwesomeIcon
                 icon={faHeartOutline}
-                onClick={() => addToFavorites(product.name)}
+                onClick={() => addToFavorites(product.id)}
                 className="heart-icon"
               />
             )}
@@ -58,14 +117,14 @@ function Favorite() {
         ))}
       </ul>
 
-      <h1 className="favorite">Favorite Products</h1>
+      <h1 className='favorite'>Favorite Products</h1>
       <ul>
-        {favorites.map((product, index) => (
+        {favorites.map((productId, index) => (
           <li key={index}>
-            {product}
+            {productId} 
             <FontAwesomeIcon
               icon={faHeartSolid}
-              onClick={() => removeFromFavorites(product)}
+              onClick={() => removeFromFavorites(productId)}
               className="heart-icon red"
             />
           </li>
