6447b17113e8a220b15203d119a77cfe56467f9a
AlirezaHK91 <kasra_tehran101@hotmail.com>
2023-10-03 23:30:31 +0200

    change the product till auction. cleaned the code a bit

--- src/components/Favourites.jsx
+++ src/components/Favourites.jsx
@@ -3,26 +3,12 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart as faHeartSolid, faHeart as faHeartOutline } from '@fortawesome/free-solid-svg-icons';
 import './Favourite.css';
 
-const products = [
-  {
-    name: "Product 1",
-    price: 19.99,
-    description: "Something",
-  },
-  {
-    name: "Product 2",
-    price: 29.99,
-    description: "Somthing",
-  }
-  
-];
-
 function Favorite() {
   
   const [favorites, setFavorites] = useState([]);
   const [userId, setUserId] = useState(null);
-  const [productId, setProductId] = useState(null);
-  const [products, setProducts] = useState([]);
+  const [auctionId, setAuctionId] = useState(null);
+  const [auctions, setAuctions] = useState([]);
 
   useEffect(() => {
     const storedUser = JSON.parse(localStorage.getItem('user'));
@@ -34,17 +20,17 @@ function Favorite() {
   
 
   useEffect(() => {
-    // Jag tänker att vi ska hämta auction istället för product, även i backenden
-    const fetchProducts = async () => {
+
+    const fetchAuction = async () => {
       try {
-        const response = await axios.get('api/get-all-products');
-        setProducts(response.data);
+        const response = await axios.get('api/auctions');
+        setAuctions(response.data);
       } catch (error) {
-        console.error('Error fetching products:', error);
+        console.error('Error fetching auctions:', error);
       }
     };
 
-    fetchProducts();
+    fetchAuction();
   }, []);
   
   useEffect(() => {
@@ -62,23 +48,21 @@ function Favorite() {
     }
   }, [userId]);
 
-  //Ska vi byta product mot auction?
   const addToFavorites = async () => {
     try {
-      const response = await axios.post('api/auth/favorite/{user_id}/{product_id}', { userId, productId });
+      const response = await axios.post('api/auth/favorite/{user_id}/{auction_id}', { userId, auctionId });
       const newFavorites = [...favorites, response.data];
       setFavorites(newFavorites);
     } catch (error) {
       console.error('Error adding to favorites:', error);
     }
   };
-  //Ska vi byta product mot auction?
-  //Vi ska också fixa remove endpoint i backenden
+
   const removeFromFavorites = async () => {
     try {
-      await axios.delete('api/auth/favorite/{id}');
+      await axios.delete('api/auth/favorite/{user_id}/{auction_id}');
 
-      const newFavorites = favorites.filter((item) => item !== productId);
+      const newFavorites = favorites.filter((item) => item !== auctionId);
       setFavorites(newFavorites);
     } catch (error) {
       console.error('Error removing from favorites:', error);
@@ -88,38 +72,26 @@ function Favorite() {
   return (
     <div>
       
-      <h1 className='products' >Products</h1>
+      <h1 className='auctions' >Auctions</h1>
       <ul>
-        {products.map((product, index) => (
+        {auctions.map((auction, index) => (
           <li key={index}>
-            {product.name}
-            {favorites.includes(product.id) ? (
-              <FontAwesomeIcon
-                icon={faHeartSolid}
-                onClick={() => removeFromFavorites(product.id)}
-                className="heart-icon red"
-              />
+            {auction.startPrice}
+            {favorites.includes(auction.id) ? (
+              <FontAwesomeIcon icon={faHeartSolid} onClick={() => removeFromFavorites(auction.id)} className="heart-icon red"/>
             ) : (
-              <FontAwesomeIcon
-                icon={faHeartOutline}
-                onClick={() => addToFavorites(product.id)}
-                className="heart-icon"
-              />
+              <FontAwesomeIcon icon={faHeartOutline} onClick={() => addToFavorites(auction.id)} className="heart-icon"/>
             )}
           </li>
         ))}
       </ul>
 
-      <h1 className='favorite'>Favorite Products</h1>
+      <h1 className='favorite'>Favorite Auctions</h1>
       <ul>
-        {favorites.map((productId, index) => (
+        {favorites.map((auctionId, index) => (
           <li key={index}>
-            {productId} 
-            <FontAwesomeIcon
-              icon={faHeartSolid}
-              onClick={() => removeFromFavorites(productId)}
-              className="heart-icon red"
-            />
+            {auctionId} 
+            <FontAwesomeIcon icon={faHeartSolid} onClick={() => removeFromFavorites(auctionId)} className="heart-icon red"/>
           </li>
         ))}
       </ul>
