2b9cf252ff6d390ae43b00d2a8922961ffbb6057
AlirezaHK91 <kasra_tehran101@hotmail.com>
2023-10-09 18:14:43 +0200

    Cleaned the code TEST

--- src/components/Pages/SubmitAuctionPage.jsx
+++ src/components/Pages/SubmitAuctionPage.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import AuctionsPage from "./AuctionsPage";
 
 const apiUrl = import.meta.env.VITE_API_URL;
 
