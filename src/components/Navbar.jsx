35e15a8c46154a082b4a88403d0f229394c4f8f1
AlirezaHK91 <kasra_tehran101@hotmail.com>
2023-10-16 15:44:53 +0200

    Deleted the android logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -152,7 +152,7 @@ function NavBar() {
                 ))}
               </Menu>
             </Box>
-            <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
+
             <Typography
               variant="h5"
               noWrap
@@ -161,7 +161,6 @@ function NavBar() {
               sx={{
                 mr: 2,
                 display: { xs: "flex", md: "none" },
-                flexGrow: 1,
                 fontFamily: "monospace",
                 fontWeight: 700,
                 letterSpacing: ".3rem",
